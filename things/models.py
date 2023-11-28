117d2fa7d4b71095b1b290e9a99b3cfa8070f814
Saihan Marshall <saihan.marshall@kcl.ac.uk>
2023-11-28 19:50:41 +0000

    Add things model

---
+++ things/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class Thing(models.Model):
+    name = models.CharField()
+    description = models.CharField()
+    quantity = models.IntegerField()
