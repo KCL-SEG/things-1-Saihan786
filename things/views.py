d429083960fd5526d02cf231e23675609f6ce938
Saihan Marshall <saihan.marshall@kcl.ac.uk>
2023-11-28 20:18:17 +0000

    Add title to response

--- things/views.py
+++ things/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    response = HttpResponse()
+    response = HttpResponse("<title>Things</title>")
     response.setdefault("h1", "Things")
     return response
