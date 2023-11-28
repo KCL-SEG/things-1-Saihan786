13760b46606e0e8b91629046d84a015b39e92fd9
Saihan Marshall <saihan.marshall@kcl.ac.uk>
2023-11-28 20:16:49 +0000

    Add setDefault and response

--- things/views.py
+++ things/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Things</h1>")
+    response = HttpResponse()
+    response.setdefault("h1", "Things")
+    return response
