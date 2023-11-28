2b58a533a1b59e54782c319725c21cc1f848ed72
Saihan Marshall <saihan.marshall@kcl.ac.uk>
2023-11-28 20:11:47 +0000

    Change to h1 from title

--- things/views.py
+++ things/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<title>Things</title>")
+    return HttpResponse("<h1>Things</h1>")
