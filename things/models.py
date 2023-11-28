e89f92e3026a1681d91549348b199eeb5f038c5e
Saihan Marshall <saihan.marshall@kcl.ac.uk>
2023-11-28 19:58:21 +0000

    Refactored Thing model

--- things/models.py
+++ things/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(models.Model):
-    name = models.CharField()
-    description = models.CharField()
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120, unique=False, blank=True)
+    quantity = models.IntegerField(unique=False, validators=[MaxValueValidator(100), MinValueValidator(0)])
